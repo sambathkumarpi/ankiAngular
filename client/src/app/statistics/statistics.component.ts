6f343f3e4e02484fb69a7fe49eead2e73b0d2ba4
sambathkumarpi <sambathkumarpi@gmail.com>
2023-12-31 12:39:43 +0100

    [DEL] unused file

--- client/src/app/statistics/statistics.component.ts
+++ client/src/app/statistics/statistics.component.ts
@@ -4,8 +4,8 @@ import { SessionService } from '../session.service';
 import * as Highcharts from 'highcharts';
 import HeatmapModule from 'highcharts/modules/heatmap';
 HeatmapModule(Highcharts);
-const chartColors = Highcharts.getOptions()?.colors;
 import { ChangeDetectorRef } from '@angular/core';
+const chartColors = Highcharts.getOptions()?.colors;
 
 @Component({
   selector: 'app-statistics',
@@ -16,56 +16,80 @@ export class StatisticsComponent implements OnInit {
   statisticsData: any[] = [];
   highcharts = Highcharts;
   chartOptions: Highcharts.Options = {};
+
   constructor(private testSessionService: SessionService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.testSessionService.getSessionData().subscribe((data) => {
       console.log('Received data:', data);
+      if (data.length === 0) {
+        console.log('No data received.');
+        return;
+      }
       this.statisticsData = data;
       this.chartOptions = this.buildChartOptions(data);
-      console.log('Chart options:', this.chartOptions);
 
       this.cdr.detectChanges();
+      console.log('Chart options:', this.chartOptions);
 
     });
   }
 
   private buildChartOptions(data: any[]): Highcharts.Options {
+    const dataPoints: any[] = [];
+    console.log('data point', dataPoints)
+    // Map SQL data to Highcharts format
+    data.forEach(session => {
+      const startTime = new Date(session.start_time);
+      const endTime = new Date(session.end_time);
+
+      // Assuming you want to consider each hour as a data point
+      for (let hour = startTime.getHours(); hour <= endTime.getHours(); hour++) {
+        const date = new Date(startTime);
+        date.setHours(hour, 0, 0, 0);
+
+        dataPoints.push([session.subject_id, date.getTime(), 1]); // The third parameter (1) represents intensity
+      }
+    });
+
     return {
       chart: {
         type: 'heatmap',
       },
       title: {
-        text: 'Test Sessions Heatmap',
+        text: 'Test Sessions Calendar Heatmap',
       },
       xAxis: {
-        categories: data.map((session) => session.subject_id.toString()),
+        type: 'category',
+        // categories: data.map((session) => session.subject_id.toString()),
         title: {
           text: 'Subject ID',
         },
       },
       yAxis: {
-        categories: data.map((session) => session.start_time),
+        type: 'datetime',
+        // categories: data.map((session) => new Date(session.start_time).toISOString()), // Use toISOString() to convert to string
         title: {
           text: 'Start Time',
         },
       },
+
       colorAxis: {
         min: 0,
         minColor: '#FFFFFF',
         maxColor: chartColors ? chartColors[0] : '#FFFFFF',
       },
-      series: [{
-        type: 'heatmap', // Add this line
-        data: data.map((session) => [session.subject_id, session.start_time, 1]),
-        borderWidth: 1,
-        dataLabels: {
-          enabled: true,
-          color: '#0FFF00',
+      series: [
+        {
+          type: 'heatmap',
+          borderWidth: 1,
+          dataLabels: {
+            enabled: true,
+            color: '#0FFF00',
+          },
+          data: dataPoints,
         },
-      }],
+      ],
     };
   }
-
-
 }
